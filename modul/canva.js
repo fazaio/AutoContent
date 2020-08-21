c2a5e6231025515f478b9f0b0598a00d9d2b838c
fazaio <faza6028@gmail.com>
2020-08-21 11:03:17 +0700

    maximum length filltext()

--- modul/canva.js
+++ modul/canva.js
@@ -2,18 +2,39 @@ const { createCanvas, loadImage } = require("canvas");
 const canvas = createCanvas(640,640);
 const ctx = canvas.getContext("2d");
 
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+async function createImg(req, res){
+    function draw(payload) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+        ctx.beginPath();
+        ctx.fillStyle= "black"
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.beginPath();
-    ctx.fillStyle= "black"
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.beginPath();
-    ctx.font = "20px Arial";
-    ctx.fillStyle= "white"
-    ctx.fillText('mengapa salah?', 10, 100)
-  
-    return canvas;
+        //  create tittle image
+        ctx.beginPath();
+        ctx.font = "36px Arial";
+        ctx.fillStyle= "white"
+        // maximum tittle length 25
+        ctx.fillText('Cara mengatasi error 1234', 25, 150)
+        ctx.fillText('pada Arduino', 25, 190)
+        let p = 'Cara mengatasi error 1234'
+        console.log(p.length);
+
+        // create content image
+        ctx.beginPath();
+        ctx.font = "24px Arial";
+        ctx.fillStyle= "red"
+
+        // maximum word length 42
+        ctx.fillText('lorem ipsum lorem ipsum lorem ipsum lorem ipsum', 40, 250)
+      
+        return canvas;
+    }
+    let data =
+    '<img src="' + draw(req.body).toDataURL("image/png") + '" />';
+    
+    res.type("text/html");
+    res.send(data);
 }
 
-module.exports = draw
+module.exports = { createImg }
--- server.js
+++ server.js
@@ -1,20 +1,14 @@
-const fastify = require('fastify')({ logger: true })
-const instagram = require('./modul/instagram')
-var draw = require('./modul/canva')
+const fastify = require("fastify")({ logger: true });
+const instagram = require("./modul/instagram");
+var draw = require("./modul/canva");
 
 
-// Route
-fastify.get('/ig/user/:name', instagram.feedUser )
-fastify.get('/ig/tags/:hashtag', instagram.feedTags )
-fastify.get('/canvas/:dynamic', async function(req, res){
-
-    let data = '<img src="' + draw().toDataURL('image/png') + '" />'
-
-    res.type('text/html')
-    res.send(data)
-    
+fastify.register(require('fastify-cors'), { 
 })
 
+// Route
+fastify.get("/ig/user/:name", instagram.feedUser);
+fastify.get("/ig/tags/:hashtag", instagram.feedTags);
+fastify.get("/createImage", draw.createImg);
 
-
-fastify.listen(3000)
+fastify.listen(3000);
